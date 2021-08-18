81fee8934d75fccdc661a561c602c77962e49cc0
Kaiyi <ZHANG.Kaiyi42@gmail.com>
2021-08-18 16:32:18 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="index-page">
+    <div class="header flex-ct-y">
+      <h1>
+        <span class="gradient">iDea CSS</span>
+      </h1>
+      <p>我是KaiyiWing，一位正在入门的CSS爱好者</p>
+      <p>欢迎来到<strong class="vue">Pure CSS World</strong></p>
+      <p>请往下滚动浏览更多好看实用的纯CSS特效👇</p>
+      <p>依据美学标准将以下纯CSS特效划分为五大类</p>
+      <p>布局-行为-色彩-图形-组件</p>
+      <p>
+        该项目为<a href="https://github.com/JowayYoung/idea-css">JowayYoung</a
+        >前辈项目的 Vue3 版本
+      </p>
+      <p>目的为学习与练习 css 技巧</p>
+    </div>
+  </div>
+  <!-- <HelloWorld msg="Hello Vue 3 + Vite" /> -->
+</template>
+
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
