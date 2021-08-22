5e07ca18c9888c2bd2893f1abe68abdacc74b12e
Kaiyi <ZHANG.Kaiyi42@gmail.com>
2021-08-22 21:36:08 +0800

    layout: flexbox 实现两列布局

--- src/components/Layout/使用flexbox排版各种布局.vue
+++ src/components/Layout/使用flexbox排版各种布局.vue
@@ -1,15 +1,28 @@
 <template>
   <div class="bruce flex-ct-x" data-title="使用flexbox排版各种布局">
     <div class="layout-box">
-      <div class="fullscreen-layout">
+      <div class="fullscreen-layout" title="全屏布局">
         <header></header>
         <main></main>
         <footer></footer>
+      </div>
+      <div class="two-column-layout" title="两列布局">
+        <div class="left"></div>
+        <div class="right"></div>
       </div>
     </div>
   </div>
 </template>
 <style lang="scss">
+.layout-box {
+  display: flex;
+  flex-direction: column;
+  > div {
+    margin: 20px 0;
+    flex-shrink: 0;
+    flex-basis: 400px;
+  }
+}
 .fullscreen-layout {
   display: flex;
   flex-direction: column;
@@ -27,6 +40,20 @@
   main {
     flex: 1;
     background-color: #3c9;
+  }
+}
+
+.two-column-layout {
+  display: flex;
+  width: 400px;
+  height: 400px;
+  .left {
+    width: 100px;
+    background-color: #f66;
+  }
+  .right {
+    flex: 1;
+    background-color: #66f;
   }
 }
 </style>
