10565cf8c775ea3870a113dd14ed0d77ea8a4bcd
Kaiyi <ZHANG.Kaiyi42@gmail.com>
2021-09-01 18:33:35 +0800

    layout: 使用 transform 描绘 0.5 px 边框

--- src/App.vue
+++ src/App.vue
@@ -25,6 +25,7 @@
     <Layout1 />
     <Layout2 />
     <Layout3 />
+    <Layout4 />
   </div>
   <div class="content behavior">
     <Behavior1 />
@@ -66,6 +67,7 @@ import { reactive, onMounted } from "vue";
 import Layout1 from "./components/Layout/使用flexbox排版各种布局.vue";
 import Layout2 from "./components/Layout/使用float排版环绕文本.vue";
 import Layout3 from "./components/Layout/使用text-overflow控制溢出文本.vue";
+import Layout4 from "./components/Layout/使用transform描绘像素边框.vue";
 import Behavior1 from "./components/Behavior/使用attr()抓取节点属性.vue";
 import Behavior2 from "./components/Behavior/使用+或~选择指定元素.vue";
 import Behavior3 from "./components/Behavior/使用@hover定制悬浮提示.vue";
---
+++ src/components/Layout/使用transform描绘像素边框.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="bruce flex-ct-y" data-title="使用transform描绘像素边框">
+    <div class="onepx-border normal">1px</div>
+    <div class="onepx-border thin">0.5px</div>
+  </div>
+</template>
+<style lang="scss" scoped>
+.onepx-border {
+  width: 200px;
+  height: 80px;
+  cursor: pointer;
+  line-height: 80px;
+  text-align: center;
+  font-weight: bold;
+  font-size: 50px;
+  color: #f66;
+  & + .onepx-border {
+    margin-top: 10px;
+  }
+  /* 设置 1px 边框时，在手机上可能看起来很粗 */
+  &.normal {
+    border: 1px solid #f66;
+  }
+  &.thin {
+    position: relative;
+    /* 使用缩放后的伪元素来实现 0.5px 像素边框 */
+    &::after {
+      position: absolute;
+      left: 0;
+      top: 0;
+      border: 1px solid #f66;
+      width: 200%;
+      height: 200%;
+      content: "";
+      transform: scale(0.5);
+      /*  
+        CSS属性让你更改一个元素变形的原点
+        x y z
+        */
+      transform-origin: left top;
+    }
+  }
+}
+</style>
